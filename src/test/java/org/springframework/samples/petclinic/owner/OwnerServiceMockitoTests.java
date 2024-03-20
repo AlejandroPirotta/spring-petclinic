e021c5bd0ce26d51593054a34de0d684b495dd84
AlejandroPirotta <alejandro.pirotta@babelgroup.com>
2024-03-20 14:04:11 +0100

    Tests

---
+++ src/test/java/org/springframework/samples/petclinic/owner/OwnerServiceMockitoTests.java
@@ -0,0 +1,42 @@
+package org.springframework.samples.petclinic.owner;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class OwnerServiceMockitoTests {
+
+	@Mock
+	private OwnerRepository ownerRepository;
+
+	@InjectMocks
+	private OwnerService ownerService;
+
+	@BeforeEach
+	public void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	public void getOwnerFullName_test() {
+		Owner owner = new Owner();
+		owner.setId(Integer.valueOf(1));
+		owner.setFirstName("Manuel");
+		owner.setLastName("Del Test");
+		owner.setAddress("Calle Virtual, 1");
+		owner.setCity("Ciudad Mock");
+		owner.setTelephone("1234567890");
+
+		Mockito.when(ownerRepository.findById(1)).thenReturn(owner);
+		String fullName = this.ownerService.getOwnerFullName(1);
+		assertEquals("Nombre completo debería ser: ", "Manuel Del Test", fullName);
+	}
+
+}
